e3c86d003ac77f0957b8111cc26569e250deb819
RODRIGO TERRIBAS SARAIVA <roterribas@uol.com.br>
2025-09-12 19:55:25 -0300

    Add files via upload

---
+++ exercicios/estruturas-de-decisao/1/main.js
@@ -0,0 +1,7 @@
+let numero = Number(prompt("Digite um número:"));
+
+if (numero > 10) {
+  alert("O número é maior que 10.");
+} else {
+  alert("O número não é maior que 10.");
+}
---
+++ exercicios/estruturas-de-decisao/6/main.js
@@ -0,0 +1,7 @@
+let nota = Number(prompt("Digite sua nota:"));
+
+if (nota >= 7.0 && nota <= 10.0) {
+  alert("A nota está entre 7.0 e 10.0.");
+} else {
+  alert("A nota não está entre 7.0 e 10.0.");
+}
